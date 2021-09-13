0f395ad5285fde19222fd37d9dda94e5b1b97f88
michaelgilliam.se <michaelgilliam.se@gmail.com>
2021-09-12 21:46:47 -0400

    more content in html and js

--- main.js
+++ main.js
@@ -11,37 +11,69 @@ function randomValueFromArray(array){
 
 // 2. RAW TEXT STRINGS
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+var storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+// added 3 sections of strings in 3 arrays
 
-the soup kitchen
-Disneyland
-the White House
+const insertX = [
+    "Willy the Goblin",
+    "Big Daddy",
+    "Father Christmas",
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
+];
+
+const insertY = [
+    "the soup kitchen",
+    "Disneyland",
+    "the White House",
+
+];
+const insertZ = [
+    "spontaneously combusted",
+    "melted into a puddle on the sidewalk",
+    "turned into a slug and crawled away",
+
+];
 
 // 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
+// below I added 4 new variables that are relevant to the arrays I made
 function result() {
+    var newStory = storyText;
+
+    var xItem = randomValueFromArray(insertX);
+    var yItem = randomValueFromArray(insertY);
+    var zItem = randomValueFromArray(insertZ);
+   
+     // below I added 4 .replace string, robin #help post, helped me!
+
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':insertx:', xItem);
+    newStory = newStory.replace(':inserty:', yItem);
+    newStory = newStory.replace(':insertz:', zItem);
+    
+    // added .replace with bob and name 
 
   if(customName.value !== '') {
     let name = customName.value;
-
+    newStory = newStory.replace('Bob', name);
+ 
   }
 
+  // added /14 for lbs to stone calculation, and added -32 * 0.5556 for F to C calculation
+  // added 2 new story.replace for the tempature and weight varibles
   if(document.getElementById("uk").checked) {
-    let weight = Math.round(300);
-    let temperature =  Math.round(94);
-
+    let weight = Math.round(300 / 14) + ' stone',
+    var temperature =  Math.round((94 - 32) * 0.5556) + ' centigrade';
+         
+   newStory = newStory.replace('94 fahrenheit', temperature);
+   newStory = newStory.replace('300 pounds', weight);
+   
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
+
 }
